080c15f8a6764343a984e9da6954b8fb5304bb45
vap1 <vaibhavvasant1705@gmail.com>
2023-08-30 19:54:42 +0530

    First Commit

--- src/apis/UserProfileApi.ts
+++ src/apis/UserProfileApi.ts
@@ -1,17 +1,18 @@
 
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { UserProfileRequest, UserProfileResponse } from '../types/Types';
 
 const getUserProfile = async (request: UserProfileRequest): Promise<UserProfileResponse> => {
   try {
-    const response = await axios.get('/api/profile', {
+    const response: AxiosResponse<UserProfileResponse> = await axios.get('/api/profile', {
       headers: {
         Authorization: `Bearer ${request.token}`,
       },
     });
     return response.data;
   } catch (error) {
-    throw new Error(error.response.data.message);
+    // Handle error
+    throw new Error('Failed to retrieve user profile');
   }
 };
 
