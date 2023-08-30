ac760bed6190e3b65977f78204ebdb8add85ead5
vap1 <vaibhavvasant1705@gmail.com>
2023-08-30 19:54:38 +0530

    First Commit

--- src/screens/RegistrationScreen.tsx
+++ src/screens/RegistrationScreen.tsx
@@ -1,30 +1,31 @@
 
 import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
-import { UserRegistrationRequest } from '../types/Types';
-import { registerUser } from '../apis/UserRegistrationApi';
+import { View, TextInput, Button, Alert } from 'react-native';
+import { UserRegistrationRequest, UserRegistrationResponse } from '../types/Types';
+import registerUser from '../apis/UserRegistrationApi';
 
 const RegistrationScreen: React.FC = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleRegistration = () => {
-    const request: UserRegistrationRequest = {
-      name,
-      email,
-      password,
-    };
-
-    registerUser(request)
-      .then((response) => {
-        console.log(response.message);
-        // Handle success, e.g., show a success message or navigate to the login screen
-      })
-      .catch((error) => {
-        console.error(error);
-        // Handle error, e.g., show an error message
-      });
+  const handleRegistration = async () => {
+    try {
+      const request: UserRegistrationRequest = {
+        name,
+        email,
+        password,
+      };
+      const response: UserRegistrationResponse = await registerUser(request);
+      if (response.success) {
+        Alert.alert('Success', response.message);
+        // Redirect to the login screen or perform any other necessary actions
+      } else {
+        Alert.alert('Error', response.message);
+      }
+    } catch (error) {
+      Alert.alert('Error', 'Failed to register user');
+    }
   };
 
   return (
@@ -41,9 +42,9 @@ const RegistrationScreen: React.FC = () => {
       />
       <TextInput
         placeholder="Password"
-        secureTextEntry
         value={password}
         onChangeText={setPassword}
+        secureTextEntry
       />
       <Button title="Register" onPress={handleRegistration} />
     </View>
