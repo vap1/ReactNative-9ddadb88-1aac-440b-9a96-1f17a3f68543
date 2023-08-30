64f25408e0ea8d24033d22159c9b36b3b0a918f1
vap1 <vaibhavvasant1705@gmail.com>
2023-08-30 19:54:39 +0530

    First Commit

--- src/screens/AdminUserDetailsScreen.tsx
+++ src/screens/AdminUserDetailsScreen.tsx
@@ -1,43 +1,51 @@
 
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { AdminUserDetailsResponse, User } from '../types/Types';
-import { getAdminUserDetails } from '../apis/AdminUserDetailsApi';
+import { View, Text, ActivityIndicator } from 'react-native';
+import { AdminUserDetailsRequest, AdminUserDetailsResponse, User } from '../types/Types';
+import getAdminUserDetails from '../apis/AdminUserDetailsApi';
 
 const AdminUserDetailsScreen: React.FC = () => {
+  const [loading, setLoading] = useState(true);
   const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
+    const fetchAdminUserDetails = async () => {
+      try {
+        const request: AdminUserDetailsRequest = {
+          token: 'YOUR_ADMIN_JWT_TOKEN', // Replace with the actual admin JWT token
+        };
+        const response: AdminUserDetailsResponse = await getAdminUserDetails(request);
+        setUsers(response.users);
+        setLoading(false);
+      } catch (error) {
+        // Handle error
+        console.error('Failed to retrieve admin user details:', error);
+        setLoading(false);
+      }
+    };
+
     fetchAdminUserDetails();
   }, []);
 
-  const fetchAdminUserDetails = async () => {
-    try {
-      const response: AdminUserDetailsResponse = await getAdminUserDetails();
-      setUsers(response.users);
-    } catch (error) {
-      console.error('Error fetching admin user details:', error);
-    }
-  };
-
-  const renderUserItem = ({ item }: { item: User }) => (
-    <View>
-      <Text>Name: {item.name}</Text>
-      <Text>Email: {item.email}</Text>
-      <Text>Contact Info: {item.contactInfo}</Text>
-      <Text>Address: {item.address}</Text>
-      <Text>Profile Picture: {item.profilePicture}</Text>
-    </View>
-  );
+  if (loading) {
+    return (
+      <View>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   return (
     <View>
-      <Text>Admin User Details</Text>
-      <FlatList
-        data={users}
-        renderItem={renderUserItem}
-        keyExtractor={(item) => item.email}
-      />
+      {users.map((user, index) => (
+        <View key={index}>
+          <Text>Name: {user.name}</Text>
+          <Text>Email: {user.email}</Text>
+          <Text>Contact Info: {user.contactInfo}</Text>
+          <Text>Address: {user.address}</Text>
+          {/* Render other user details */}
+        </View>
+      ))}
     </View>
   );
 };
