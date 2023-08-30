9dcb0a8959f1d9fba53a6f17f606cb1cc09dbbcb
vap1 <vaibhavvasant1705@gmail.com>
2023-08-30 19:54:46 +0530

    First Commit

--- src/apis/AdminUserDetailsApi.ts
+++ src/apis/AdminUserDetailsApi.ts
@@ -1,17 +1,18 @@
 
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { AdminUserDetailsRequest, AdminUserDetailsResponse } from '../types/Types';
 
 const getAdminUserDetails = async (request: AdminUserDetailsRequest): Promise<AdminUserDetailsResponse> => {
   try {
-    const response = await axios.get('/api/admin/users', {
+    const response: AxiosResponse<AdminUserDetailsResponse> = await axios.get('/api/admin/users', {
       headers: {
         Authorization: `Bearer ${request.token}`,
       },
     });
     return response.data;
   } catch (error) {
-    throw new Error(error.response.data.message);
+    // Handle error
+    throw new Error('Failed to retrieve admin user details');
   }
 };
 
