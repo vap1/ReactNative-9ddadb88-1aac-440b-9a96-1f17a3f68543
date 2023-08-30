be9a756da42a20f9f9f48c98d349e3a891210bf4
vap1 <vaibhavvasant1705@gmail.com>
2023-08-30 19:54:37 +0530

    First Commit

--- src/apis/UserProfileUpdateApi.ts
+++ src/apis/UserProfileUpdateApi.ts
@@ -1,13 +1,18 @@
 
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { UserProfileUpdateRequest, UserProfileUpdateResponse } from '../types/Types';
 
 const updateUserProfile = async (request: UserProfileUpdateRequest): Promise<UserProfileUpdateResponse> => {
   try {
-    const response = await axios.put('/api/profile', request);
+    const response: AxiosResponse<UserProfileUpdateResponse> = await axios.put('/api/profile', request, {
+      headers: {
+        Authorization: `Bearer ${request.token}`,
+      },
+    });
     return response.data;
   } catch (error) {
-    throw new Error(error.response.data.message);
+    // Handle error
+    throw new Error('Failed to update user profile');
   }
 };
 
